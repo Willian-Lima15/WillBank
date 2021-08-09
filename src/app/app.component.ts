a3df8120932e53cbeeda596290dfe4c719d411b7
Willian Suporte7v <william_slayer@hotmail.com>
2021-08-09 08:12:25 -0300

    Sitema de transferência finalizado

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'willbank';
+
+  constructor(private service: TransferenciaService) {
+
+  }
+
+}
